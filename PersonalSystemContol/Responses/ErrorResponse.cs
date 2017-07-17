be3fb73abf3e952a4818f69c57f6c8454d10ceb2
Denchik <denchik_mih@mail.ru>
2017-07-17 11:28:28 +0700

    git add

---
+++ PersonalSystemContol/Responses/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonalSystemContol.Responses
+{
+    /// <summary>
+    /// Генерация ответа с информацией об ошибке.
+    /// </summary>
+    public class ErrorResponse
+    {
+        public string status;
+        public string error;
+
+        public ErrorResponse(string status, string error)
+        {
+            this.status = status;
+            this.error = error;
+        }
+    }
+}
---
+++ PersonalSystemContol/Responses/StandartResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonalSystemContol.Responses
+{
+    /// <summary>
+    /// Генерация обычного ответа.
+    /// </summary>
+    public class StandartResponse
+    {
+        public string status;
+        public int id;
+
+        public StandartResponse(string status, int id)
+        {
+            this.status = status;
+            this.id = id;
+        }
+    }
+}
